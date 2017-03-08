9d2309c05e512988f3e5ba743bc60971d011e9d1
徐泽宇 <xzy@9elephas.com>
2017-03-08 16:00:33 +0800

    update controller

--- tct/src/main/java/com/ninelephas/tct/controller/UserController.java
+++ tct/src/main/java/com/ninelephas/tct/controller/UserController.java
@@ -1,7 +1,18 @@
+/*
+ * Copyright 九象网络科技（上海）有限公司 (c) 2017.
+ *
+ * author： 徐泽宇(roamer)
+ *
+ * class: com.ninelephas.tct.controller.UserController
+ *
+ * created： 2017-03-08 11:45
+ * lastModified： 2017-03-08 11:45
+ *
+ */
+
 package com.ninelephas.tct.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,46 +21,25 @@ import org.springframework.web.servlet.ModelAndView;
 
 
 /**
- * 
- * @ClassName: UserController
- * @Description: User的mvc控制器
- * @author 徐泽宇
- * @date 2016年12月28日 下午6:24:05
  *
  */
 @Controller("com.ninelephas.tct.controller.UserController")
 @RequestMapping(value = "/user")
+@Log4j2
 public class UserController {
 
-    /**
-     * Logger for this class
-     */
-    private static final Logger logger = LogManager.getLogger(UserController.class.getName());
 
 
     /**
-     * 
-     * login
-     * 
-     * @Auther 徐泽宇
-     * @Date 2016年12月28日 下午6:26:57
-     * @Title: login
-     * @Description: 用户登录
+     *
      * @param userName
      * @param oPasswd
      * @return
-     * @throws ControllerException
      */
-    @RequestMapping(value = "/login", produces = "application/json;charset=utf-8", method = RequestMethod.POST)
+    @RequestMapping(value = "/login", produces = "application/json;charset=utf-8", method = RequestMethod.GET)
     @ResponseBody
-    public ModelAndView login(String userName, String oPasswd)  {
-        if (logger.isDebugEnabled()) {
-            logger.debug("login(String userName={}, String oPasswd={}) - start", userName, oPasswd); //$NON-NLS-1$
-        }
-
-        if (logger.isDebugEnabled()) {
-            logger.debug("login(String userName={}, String oPasswd={}) - end", userName, oPasswd); //$NON-NLS-1$
-        }
+    public ModelAndView login(String userName, String oPasswd) {
+        log.debug("开始进行登录处理！");
         ModelAndView mv = new ModelAndView();
         mv.setViewName("/index");
         return mv;
